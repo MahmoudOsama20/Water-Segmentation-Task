9c6e9539b59687f73c3d7be709330a4e46b1d7c1
MahmoudOsama20 <mo2314151@gmail.com>
2026-03-04 14:12:03 +0200

    model.py added

---
+++ App with Flask/model.py
@@ -0,0 +1,26 @@
+import torch
+import segmentation_models_pytorch as smp
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def load_model(model_path="water_model.pth"):
+
+    # checkpoint = torch.load(model_path, map_location=DEVICE)
+    checkpoint = torch.load(
+    model_path,
+    map_location=DEVICE,
+    weights_only=False  # allow full checkpoint loading
+)
+
+    model = smp.DeepLabV3(
+        encoder_name="efficientnet-b7",
+        encoder_weights=None,
+        in_channels=12,
+        classes=1
+    )
+
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.to(DEVICE)
+    model.eval()
+
+    return model, checkpoint["band_mins"], checkpoint["band_maxs"]
